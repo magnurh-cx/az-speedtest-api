d00183a1f0620d43d60a0c3f1a5bb85f6cf25eaa
Magnus Røed Hestvik <magnus.roed.hestvik@computas.com>
2020-02-12 14:42:35 +0100

    Transmit events to Azure EventHubs

--- SpeedTestApi/Controllers/SpeedTestController.cs
+++ SpeedTestApi/Controllers/SpeedTestController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 using SpeedTestApi.Models;
+using SpeedTestApi.Services;
 
 namespace SpeedTestApi.Controllers
 {
@@ -9,10 +11,12 @@ namespace SpeedTestApi.Controllers
     public class SpeedTestController : ControllerBase
     {
 
-        private ILogger _logger;
+        private readonly ILogger _logger;
+        private readonly ISpeedTestEvents _eventHub;
 
-        public SpeedTestController(ILogger<SpeedTestController> logger){
+        public SpeedTestController(ILogger<SpeedTestController> logger, ISpeedTestEvents eventHub){
             _logger = logger;
+            _eventHub = eventHub;
         }
 
         // GET speedtest/ping
@@ -25,8 +29,10 @@ namespace SpeedTestApi.Controllers
 
         // POST speedtest/
         [HttpPost]
-        public string UploadSpeedTest([FromBody] TestResult speedTest)
+        public async Task<string> UploadSpeedTest([FromBody] TestResult speedTest)
         {
+            await _eventHub.PublishSpeedTest(speedTest);
+
             var response = $"Got a TestResult from { speedTest.User } with download { speedTest.Data.Speeds.Download } Mbps.";
             _logger.LogInformation(response);
 
