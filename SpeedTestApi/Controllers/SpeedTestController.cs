813ee0945a41993d7c11d06f94141828124cb48b
Magnus Røed Hestvik <magnus.roed.hestvik@computas.com>
2020-02-12 11:32:13 +0100

    First version of speed-test-api

---
+++ SpeedTestApi/Controllers/SpeedTestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SpeedTestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SpeedTestController : ControllerBase
+    {
+        // GET speedtest/ping
+        [Route("ping")]
+        [HttpGet]
+        public string Ping()
+        {
+            return "PONG";
+        }
+    }
+}
